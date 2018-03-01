437a2a405ecc262ea25185d75ee50016435f29b2
Michelle Madden <maddenmichelle@yahoo.com>
2018-02-28 19:17:15 -0500

    Clean up some small errors

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -68,6 +68,8 @@ class OrdersController < ApplicationController
     def ensure_cart_isnt_empty
       if @cart.line_items.empty?
         redirect_to store_index_url, notice: 'Your cart is empty'
+      end
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_order
       @order = Order.find(params[:id])
