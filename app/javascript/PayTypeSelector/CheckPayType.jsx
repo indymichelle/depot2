d4e8f756fb86660dac88cb826116407730aac843
Michelle Madden <maddenmichelle@yahoo.com>
2018-03-24 18:55:39 -0400

    Add system test to check functionality

--- app/javascript/PayTypeSelector/CheckPayType.jsx
+++ app/javascript/PayTypeSelector/CheckPayType.jsx
@@ -6,9 +6,17 @@ class CheckPayType extends React.Component {
 			<div>
 				<div className="field">
 					<label htmlFor="order_routing_number">Routing #</label>
+					<input type="password"
+							 	 name="order[routing_number]"
+							 	 id="order_routing_number" />
+				</div>
+				<div className="field">
+					<label htmlFor="order_account_number">Account #</label>
 					<input type="text"
 							 	 name="order[account_number]"
 							 	 id="order_account_number" />
+
+
 				</div>
 			</div>
 		);
